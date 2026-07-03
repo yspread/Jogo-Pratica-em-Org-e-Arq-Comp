e37140ad830e7c46438a8b8bdfb154ce625c9db7
Hidden-cel <82791557+EricCL6@users.noreply.github.com>
2026-07-03 12:10:27 -0300

    n lembro oq q eu fiz

--- campo_minado/jogo.c
+++ campo_minado/jogo.c
@@ -1,8 +1,16 @@
+//Dimensões da tela
 #define LARGURA 40
-#define ESPACO 32
-#define BOARDLENGTH 19
-#define BOARDHEIGHT 14
-#define NBOMBAS 32
+#define ALTURA 30
+//Dimensões do tabuleiro
+#define BOARDLARGURA 19
+#define BOARDALTURA 14
+#define NBOMBAS 40
+//Valores dos chars de movimento
+#define CIMA 70
+#define BAIXO 72
+#define DIR 71
+#define ESQ 69
+
 
 void print_str(char *texto, short pos) {
     short i;
@@ -24,43 +32,54 @@ void clearScreen() {
 }
 
 void espera_espaco() {
-    short tecla;
+    char tecla;
 
-    tecla = 0;
-    while (tecla != ESPACO) {
+    char tecla = 0;
+    while (tecla != ' ') {
         tecla = _inchar();
     }
 }
 
+/*
 // Função retorna um tabuleiro novo
 char* make_tabuleiro() {
     //Cria uma tabela de indices para a lógica do fisher yates
-    short indices[BOARDLENGTH*BOARDHEIGHT];
-    char mapa[BOARDLENGTH*BOARDHEIGHT];
+    short indices[BOARDLARGURA*BOARDALTURA];
+    char* mapa = malloc(sizeof(char) * BOARDLARGURA*BOARDALTURA);
 
-    for(short i = 0; i < BOARDLENGTH*BOARDHEIGHT; i++)indices[i] = i;
-
+    for(short i = 0; i < BOARDLARGURA*BOARDALTURA; i++)indices[i] = i;
+    
+    srand(10);
     for(short i = 0; i < NBOMBAS; i++){
-        short r = rand();
-        r %= BOARDLENGTH*BOARDHEIGHT;
-        mapa[r] = '-1';
+        unsigned short r = rand();
+        r %= BOARDLARGURA*BOARDALTURA - 1;
+        mapa[r + 1] = '9';
         short troca = indices[i];
         indices[i] = indices[r];
         indices[r] = troca;
     }
-    for(short i = 0; i < BOARDLENGTH*BOARDHEIGHT; i++){
-        if(mapa[i] == -1)continue;
+    for(short i = 0; i < BOARDLARGURA*BOARDALTURA; i++){
+        if(mapa[i] == '9')continue;
         mapa[i] = '0';
         short dx[8] = {0, 1, 0, -1, 1, 1, -1, -1};
         short dy[8] = {1, 0, -1, 0, -1, 1, -1, 1};
-        short x = i%BOARDLENGTH;
-        short y = i/BOARDLENGTH;
+        short x = i%BOARDLARGURA;
+        short y = i/BOARDLARGURA;
         for(short j = 0; j < 8; j++){
-            if(x + dx[j] < 0 || BOARDLENGTH <= x + dx[j])continue;
-            if(y + dy[j] < 0 || BOARDHEIGHT <= y + dy[j])continue;
-            short idViz = (y + dy[j])*BOARDLENGTH + x + dx[j];
-            if(mapa[idViz] == -1)mapa[i]++;
+            if(x + dx[j] < 0 || BOARDLARGURA <= x + dx[j])continue;
+            if(y + dy[j] < 0 || BOARDALTURA <= y + dy[j])continue;
+            short idViz = (y + dy[j])*BOARDLARGURA + x + dx[j];
+            if(mapa[idViz] == '9')mapa[i]++;
         }
+    }
+    return mapa;
+}
+*/
+char* make_tabuleiro() {
+    char* mapa = malloc(sizeof(char) * BOARDLARGURA*BOARDALTURA);
+    char ref[BOARDALTURA*BOARDLARGURA] = {'1', '3', '9', '3', '9', '1', '0', '1', '1', '1', '1', '1', '1', '0', '0', '0', '0', '0', '0', '1', '9', '9', '4', '2', '2', '0', '1', '9', '1', '1', '9', '1', '1', '1', '1', '0', '1', '1', '2', '3', '2', '2', '9', '2', '1', '2', '1', '1', '1', '1', '1', '1', '9', '1', '0', '2', '9', '9', '1', '0', '1', '1', '2', '9', '2', '1', '2', '1', '1', '0', '1', '1', '1', '0', '2', '9', '1', '1', '0', '0', '0', '1', '1', '2', '9', '3', '9', '2', '0', '0', '0', '1', '1', '2', '1', '1', '1', '1', '0', '0', '0', '1', '2', '2', '3', '9', '3', '1', '1', '1', '2', '9', '1', '0', '1', '9', '1', '0', '0', '0', '1', '9', '1', '1', '2', '9', '1', '1', '9', '2', '1', '1', '0', '1', '1', '2', '1', '1', '0', '1', '1', '1', '0', '1', '2', '2', '2', '1', '2', '1', '1', '0', '0', '0', '1', '9', '2', '1', '1', '0', '0', '0', '0', '2', '9', '3', '1', '2', '9', '1', '0', '1', '1', '1', '1', '2', '9', '1', '0', '0', '0', '0', '2', '9', '4', '9', '3', '2', '2', '0', '9', '2', '0', '0', '1', '1', '1', '1', '1', '1', '0', '1', '2', '9', '2', '2', '9', '1', '0', '9', '3', '0', '1', '2', '2', '1', '1', '9', '2', '1', '1', '2', '3', '3', '3', '2', '1', '0', '9', '2', '0', '1', '9', '9', '1', '1', '2', '9', '1', '1', '9', '2', '9', '9', '1', '0', '0', '1', '1', '0', '1', '2', '2', '1', '0', '1', '1', '1', '1', '1', '2', '2', '2', '1', '0', '0'};
+    for(int i = 0; i < BOARDALTURA*BOARDLARGURA; i++){
+        mapa[i] = ref[i];
     }
     return mapa;
 }
@@ -70,9 +89,9 @@ void revelar_recursivo(short pos, char* revelado, char* mapa) {
     //Paramos caso a casa já havia sido revelada
     if(revelado[pos] == '1')return;
     revelado[pos] = '1';
-    short x = pos%BOARDLENGTH;
-    short y = pos/BOARDLENGTH;
-    _outchar(mapa[pos], 80 * y + 2 * x + 81);
+    short x = pos%BOARDLARGURA;
+    short y = pos/BOARDLARGURA;
+    _outchar(mapa[pos], 2 * (BOARDLARGURA + 1) * (y + 1) + 2 * x);
 
     //Não revelamos os vizinhos de casas não nulas
     if(mapa[pos] != '0')return;
@@ -81,21 +100,42 @@ void revelar_recursivo(short pos, char* revelado, char* mapa) {
     short dx[8] = {0, 1, 0, -1, 1, 1, -1, -1};
     short dy[8] = {1, 0, -1, 0, -1, 1, -1, 1};
     for(short j = 0; j < 8; j++){
-        if(x + dx[j] < 0 || BOARDLENGTH <= x + dx[j])continue;
-        if(y + dy[j] < 0 || BOARDHEIGHT <= y + dy[j])continue;
-        short vizPos = (y + dy[j])*BOARDLENGTH + x + dx[j];
+        if(x + dx[j] < 0 || BOARDLARGURA <= x + dx[j])continue;
+        if(y + dy[j] < 0 || BOARDALTURA <= y + dy[j])continue;
+        short vizPos = (y + dy[j])*BOARDLARGURA + x + dx[j];
         revelar_recursivo(vizPos, revelado, mapa);
     }
 }
+void start_game(){
+    char* tabuleiro = make_tabuleiro();
+    print_str("BOMBAS: 40", 0);
 
-int main() {
+    short tab_pos;
+
+    while(1){
+        char tecla = _inchar();
+        switch(tecla){
+        case CIMA:
+            
+            break;
+        
+        default:
+            break;
+        }
+    }
+}
+void menu(){
     print_str("CAMPO MINADO", 3 * LARGURA + 14);
     print_str("PRESSIONE ESPACO PARA JOGAR", 6 * LARGURA + 6);
 
     espera_espaco();
     clearScreen();
+}
 
-    print_str("BOMBAS: 10", 0);
+int main() {
+    menu();
+
+    
 
     while (1) {
 
